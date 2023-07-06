a361dc6d3afa57bb372328f00ece69c181e60936
Christian Vega <cvega@darkware.net>
2023-07-06 12:48:34 -0500

    i fixed my codding problem

--- Assets/Script/score kepper.cs
+++ Assets/Script/score kepper.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class scorekepper : MonoBehaviour
+public class ScoreKeeper : MonoBehaviour
 {
     int correctAnswers = 0;
     int questionsSeen = 0;
